e7055c48eb951468f2b2891f142efd7c6c10ae73
ekkk1126 <cabi0203@naver.com>
2026-01-21 00:12:03 +0900

    feat: 루트라우터 와 네비게이트 라우터 분리

--- src/routes/RootRoute.tsx
+++ src/routes/RootRoute.tsx
@@ -3,36 +3,40 @@ import LoginPage from '@/pages/LoginPage';
 import MainPage from '@/pages/MainPage';
 import MyPage from '@/pages/MyPage';
 import SignupPage from '@/pages/SignupPage';
-import ShoppingCartPage from '@/pages/ShoppingCartPage';
+import ShoppingCartPage from '@/pages/CartPage';
 import CheckoutPage from '@/pages/CheckoutPage';
-import ProductListPage from '@/pages/ProductListPage';
 import ProductDetailPage from '@/pages/ProductDetailPage';
 import LLMSearchResultPage from '@/pages/LLMSearchResultPage';
 import ShoppingResearchPage from '@/pages/ShoppingResearchPage';
 import ShoppingResearchResultPage from '@/pages/ShoppingResearchResultPage';
-import { PATH } from '@/routes/path';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import TokenChargePage from '@/components/myPage/users/TokenChargePage';
 import MyTimerGrid from '@/components/myPage/timer/MyTimerGrid';
+import { ROUTE_PATH } from '@/routes/path';
 
 const RootRoute = () => {
   return (
     <Routes>
       <Route element={<GlobalLayout />}>
-        <Route path={PATH.ROOT} element={<MainPage />} />
-        <Route path={PATH.LOGIN} element={<LoginPage />} />
-        <Route path={PATH.SIGNUP} element={<SignupPage />} />
-        <Route path={PATH.CART} element={<ShoppingCartPage />} />
-        <Route path={PATH.CHECKOUT} element={<CheckoutPage />} />
-        <Route path={PATH.PRODUCT_LIST} element={<ProductListPage />} />
-        <Route path={PATH.PRODUCT_DETAIL} element={<ProductDetailPage />} />
-        <Route path={PATH.LLM_SEARCH_RESULT} element={<LLMSearchResultPage />} />
-        <Route path={PATH.SHOPPING_RESEARCH} element={<ShoppingResearchPage />} />
-        <Route path={PATH.SHOPPING_RESEARCH_RESULT} element={<ShoppingResearchResultPage />} />
-        <Route path={PATH.MY_PAGE} element={<MyPage />}>
-          <Route index element={<Navigate to={PATH.TIMER} replace />} />
-          <Route path={PATH.TIMER} element={<MyTimerGrid />} />
-          <Route path={PATH.TOKEN} element={<TokenChargePage />} />
+        <Route path={ROUTE_PATH.ROOT} element={<MainPage />} />
+        <Route path={ROUTE_PATH.LOGIN} element={<LoginPage />} />
+        <Route path={ROUTE_PATH.SIGNUP} element={<SignupPage />} />
+        <Route path={ROUTE_PATH.CART} element={<ShoppingCartPage />} />
+        <Route path={ROUTE_PATH.CHECKOUT} element={<CheckoutPage />} />
+
+        <Route path={ROUTE_PATH.PRODUCT_DETAIL} element={<ProductDetailPage />} />
+
+        <Route path={ROUTE_PATH.LLM_SEARCH_RESULT} element={<LLMSearchResultPage />} />
+        <Route path={ROUTE_PATH.SHOPPING_RESEARCH} element={<ShoppingResearchPage />} />
+        <Route
+          path={ROUTE_PATH.SHOPPING_RESEARCH_RESULT}
+          element={<ShoppingResearchResultPage />}
+        />
+
+        <Route path={ROUTE_PATH.MY_PAGE} element={<MyPage />}>
+          <Route index element={<Navigate to={ROUTE_PATH.TIMER} replace />} />
+          <Route path={ROUTE_PATH.TIMER} element={<MyTimerGrid />} />
+          <Route path={ROUTE_PATH.TOKEN} element={<TokenChargePage />} />
         </Route>
       </Route>
     </Routes>
--- src/routes/path.ts
+++ src/routes/path.ts
@@ -1,6 +1,5 @@
-export const PATH = {
+export const ROUTE_PATH = {
   ROOT: '/',
-
   LOGIN: '/login',
   SIGNUP: '/signup',
 
@@ -9,6 +8,7 @@ export const PATH = {
 
   PRODUCT_LIST: '/products',
   PRODUCT_DETAIL: '/products/:id',
+
   LLM_SEARCH_RESULT: '/search/llm/:keyword',
   SHOPPING_RESEARCH: '/shopping-research/:keyword',
   SHOPPING_RESEARCH_RESULT: '/shopping-research-result/:keyword',
@@ -16,4 +16,24 @@ export const PATH = {
   MY_PAGE: '/mypage',
   TIMER: 'timer',
   TOKEN: 'token',
+};
+
+export const PATH = {
+  ROOT: '/',
+  LOGIN: '/login',
+  SIGNUP: '/signup',
+
+  CART: '/cart',
+  CHECKOUT: '/checkout',
+
+  PRODUCT_LIST: '/products',
+
+  PRODUCT_DETAIL: (id: number | string) => `/products/${id}`,
+  LLM_SEARCH_RESULT: (keyword: string) => `/search/llm/${keyword}`,
+  SHOPPING_RESEARCH: (keyword: string) => `/shopping-research/${keyword}`,
+  SHOPPING_RESEARCH_RESULT: (keyword: string) => `/shopping-research-result/${keyword}`,
+
+  MY_PAGE: '/mypage',
+  TIMER: '/mypage/timer',
+  TOKEN: '/mypage/token',
 };
